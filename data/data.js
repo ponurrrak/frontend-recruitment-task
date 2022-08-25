4744f037ba321ba56f4e309f31ba9aa588810d31
Maciek Mielczarek <maciejka-67@o2.pl>
2022-08-25 10:02:39 +0200

    Fix bugs in styles

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 export default {
-  photoFileName: 'sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_4_3,c_fill,g_auto__c_scale,w_538.jpg',
+  photoFileName: 'sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg',
   photoAlt: 'Summer sunset on the beach',
-  title: 'Lorem ipsum',
+  title: 'Lorem Ipsum',
   text: 'Infinitely scalable, feature-rich and cloud-native data management and protection for modern and legacy infrastructures and SaaS platforms, managed via a single app with no hardware required.',
   buttonText: 'Button',
 };
